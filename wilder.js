d992f93f962b9b3338c1fffbb35331853353b562
Sebastien Flamant <sebastienflamant31@gmail.com>
2019-03-20 15:01:13 +0100

    atelier node js

---
+++ wilder.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Denis'));
+console.log(chalk.white('Sébastien'));
+console.log(chalk.red('Anisse'));
+console.log(chalk.yellow('Arnaud'));
+console.log(chalk.green('Sandy'));
